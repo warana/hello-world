1360ba9f01323b02da9bd598e68e908421fe4145
warana <warana.ko@gmail.com>
2021-08-01 19:24:25 +0900

    Create mpu6050.h

---
+++ mpu6050.h
@@ -0,0 +1,20 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: 2021. 8. 1.
+ *      Author: k2
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+#define DEVICE_ADDRESS	0x68;	// Device Address
+#define PWR_MGMT_1		0x6B;	// Power Management 1 Register
+#define CONFIG			0x1A;	// Configuration
+#define SMPRT_DIV		0x19;	// Sample Rate Divider
+
+
+
+
+
+#endif /* INC_MPU6050_H_ */
